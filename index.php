5f2e26c4c7ee85a13856f3348d2efcbcee7c3f12
Doni <tambah file html dan css>
2024-09-20 00:45:25 +0900

    tambahkan file CSS

--- index.php
+++ index.php
@@ -21,27 +21,41 @@
 
     <style>
         body{
-            background-image: url('https://img.freepik.com/free-photo/cloud-forest-landscape_23-2151794681.jpg?t=st=1726743441~exp=1726747041~hmac=740a9a9e6442ceac0ded0eb57c0a3e0e9bb8a66547d97590e85cb1e1c26cb36d&w=826');
+            background-image: url('https://img.freepik.com/free-photo/milky-way-galaxy-shines-tranquil-pond-generated-by-ai_188544-22770.jpg?t=st=1726759154~exp=1726762754~hmac=8f9c34a33f79919a66a6e776f6e1168b88273caa880d5c8f65b65f74b334a00c&w=740');
             background-size: cover;
             background-position: center;
             height: 100vh;
+        }
+
+        .border {
+            background-color: transparent;
+             border: none;
+    outline: none;
+    border: 2px solid rgba(255, 255, 255, .2);
+    border-radius: 40px;
+    font-size: 16px;
+    color: #fff;
+    padding: 20px 45px 20px 20px;
+    width: 100;
         }
     </style>
 
 </head>
 <body class="">
     <div class="container d-flex justify-content-center align-items-center vh-100">
-        <div class="card p-4 shadow" style="max-width : 400px; width: 100%">
-            <h3 class="text-center mb-4 fw-bold">LOGIN</h3>
+        <div class="card p-4 shadow;" style="max-width : 300px; width: 100%; background: transparent;  border-radius: 12px;
+  color: #fff;
+  border: 2px solid rgba(255, 255, 255, .2);  backdrop-filter: blur(10px);">
+            <h3 class="text-center mb-4 fw-bold ">LOGIN</h3>
             <form action="" method="POST">
                 <div class="mb-3">
                     <label for="email" class="form-label">Email </label>
-                    <input type="email" name="email" class="form-control" id="email" placeholder="Masukan Email"  required>
+                    <input class="border" type="email" name="email" class="form-control" id="email" placeholder="donilokobal@gmail.com"  required>
                 </div>
 
                 <div class="mb-3">
                     <label for="password" class="form-label">Password </label>
-                    <input type="password" name="password" class="form-control" id="password"  required>
+                    <input class="border" type="password" name="password" class="form-control" id="password" placeholder="Your Password" required>
                 </div>
                 <button type="submit"  class="btn btn-primary w-100"> Login</button>
             </form>
